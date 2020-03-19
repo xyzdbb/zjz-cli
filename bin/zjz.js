dd29a4fc2f55d83bb04f481f7a96c36b09ef1d92
yearnZero <azhensmile@gmail.com>
2020-03-19 15:12:36 +0800

    Refactor command

---
+++ bin/zjz.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+const program = require('commander');
+program.version(require('../package').version).usage('<command> [options]');
+
+program
+  .command('add')
+  .alias('a')
+  .description('add a new template')
+  .action(() => require('../lib/command/add'));
+
+program
+  .command('delete')
+  .alias('d')
+  .description('delete a template')
+  .action(() => require('../lib/command/delete'));
+
+program
+  .command('list')
+  .alias('ls')
+  .description('list all the templates')
+  .action(() => require('../lib/command/list'));
+program
+  .command('init')
+  .alias('i')
+  .description('generate a new project from a template')
+  .action(() => require('../lib/command/init'));
+
+program.parse(process.argv);
+
+// if (!program.args.length) {
+//   program.help();
+// }
